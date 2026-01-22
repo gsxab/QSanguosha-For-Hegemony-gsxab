c3cd3ae8bf03ca6ca500c99cf8c1d161a6c2cb5f
gsxab <2072414837@qq.com>
2026-01-22 22:58:06 +0800

    adjust ai for chuanxin

--- lua/ai/momentum-ai.lua
+++ lua/ai/momentum-ai.lua
@@ -458,11 +458,16 @@ end
 --张任
 sgs.ai_skill_invoke.chuanxin = function(self, data)
 	local damage = data:toDamage()
+	if self:isFriend(damage.to) then
+		return false
+	end
 	if damage.to:hasShownSkill("niepan") and not damage.to:inHeadSkills("niepan") and damage.to:getMark("@nirvana") > 0 then
 		return true
 	end
-	return not self:isFriend(damage.to)
-		and not self:hasHeavySlashDamage(self.player, damage.card, damage.to)
+	if damage.to:hasShownSkill("buqu") and not damage.to:inHeadSkills("buqu") and damage.to:getPile("scars"):length() <= 4 then
+		return true
+	end
+	return not self:hasHeavySlashDamage(self.player, damage.card, damage.to)
 		and not (damage.to:getHp() == 1 and not damage.to:getArmor())
 end
 
