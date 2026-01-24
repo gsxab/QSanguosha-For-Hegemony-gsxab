ec15fafd192b58328e30eada4e8d0e373ea725d5
gsxab <2072414837@qq.com>
2026-01-24 19:59:27 +0800

    adjust ai for jianglve

--- lua/ai/power-ai.lua
+++ lua/ai/power-ai.lua
@@ -277,11 +277,12 @@ jianglve_skill.getTurnUseCard = function(self, inclusive)
 			friend_count = friend_count + 1
 		end
 	end
-	if friend_count * 3 <= #self.room:getAlivePlayers() and unknown_count > friend_count * 2 then
-		return
+	if friend_count * 3 > #self.room:getAlivePlayers() or unknown_count <= friend_count * 2 then
+		return sgs.Card_Parse("@JianglveCard=.&jianglve")
 	end
-
-	return sgs.Card_Parse("@JianglveCard=.&jianglve")
+	if string.find(sgs.gameProcess(), "shu>>>") then
+		return sgs.Card_Parse("@JianglveCard=.&jianglve")
+	end
 end
 
 sgs.ai_skill_use_func.JianglveCard= function(card, use, self)
